f323418a3926fcb2c99a40b44aee235198ebe194
Vasilis321 <wolpina2006@gmail.com>
2026-02-25 19:15:29 +0700

    FixAllErrors

---
+++ src/views/CartView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cart">
+    <h1>Корзина</h1>
+    <p>Здесь будет список товаров в корзине</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartView'
+}
+</script>
+
+<style scoped>
+.cart {
+  padding: 20px;
+}
+</style>
---
+++ src/views/OrdersView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="orders">
+    <h1>Мои заказы</h1>
+    <p>Здесь будет список заказов</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrdersView'
+}
+</script>
+
+<style scoped>
+.orders {
+  padding: 20px;
+}
+</style>
